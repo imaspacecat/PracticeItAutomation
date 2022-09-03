0a3383bea41ffcb823b339f6405d194552cb968f
imaspacecat <library4lion@gmail.com>
2022-09-03 01:13:54 -0700

    eh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.html.parser.HTMLParser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     // self check - https://www.buildingjavaprograms.com/self-check-solutions-4ed.html
@@ -8,17 +15,45 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         try (final WebClient webClient = new WebClient()) {
-            final HtmlPage scSolutions = webClient.getPage(
-                    "https://www.buildingjavaprograms.com/self-check-solutions-4ed.html");
-            String html = scSolutions.asXml();
-            String[] chapters = html.split("^.*<h2.*$", -1);
-            for (int i = 0; i < chapters.length; i++) {
-                System.out.println(i);
-                System.out.println(chapters[i]);
-            }
-            System.out.println(chapters.length);
+            final String email = "imaspacecatperson@gmail.com";
+            final String password = "thisisthepasswordbecareful";
+
+            final HtmlPage loginPage = webClient.getPage(
+                    "https://practiceit.cs.washington.edu/login");
+
+            final HtmlForm loginForm = loginPage.getHtmlElementById("loginform");
+
+            HtmlTextInput userField = loginForm.getInputByName("usernameoremail");
+            HtmlPasswordInput passField = loginForm.getInputByName("password");
+
+            userField.type(email);
+            passField.type(password);
+
+            HtmlSubmitInput submitInput = loginForm.getInputByName("login");
+            HtmlPage inboxPage = submitInput.click();
+            HtmlPage problems = webClient.getPage("https://practiceit.cs.washington.edu/problem/list");
+            inboxPage.save(new File("inbox.html"));
+
 
         }
+//        Document scSolutions = Jsoup.connect(
+//                "https://www.buildingjavaprograms.com/self-check-solutions-4ed.html").get();
+//
+//        String html = scSolutions.html();
+//        String[] chapters = html.split("<h2");
+//
+//        Document[] chapterDocs = new Document[chapters.length];
+//
+//        for (int i = 0; i < chapters.length; i++) {
+//            System.out.println("---------------------");
+//            System.out.println(chapters[i]);
+//            chapterDocs[i] = Jsoup.parse(chapters[i]);
+//        }
+//        System.out.println("+++++++++++++++++");
+//        System.out.println(chapterDocs[1].html());
+
+
+
 
     }
 
