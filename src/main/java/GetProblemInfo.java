f4ccc8d9ef9aa2bc32c199f862d353ae7144d6d6
brin <itsbrian1113@gmail.com>
2022-09-05 15:27:49 -0700

    hi yotam

---
+++ src/main/java/GetProblemInfo.java
@@ -0,0 +1,118 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class GetProblemInfo {
+    public static void main(String[] args) throws IOException {
+
+        GetProblemInfo get = new GetProblemInfo();
+
+        List<String[]> problemInfo = get.getProblemInfo();
+
+        FileWriter file = new FileWriter("src/main/problemInfo");
+        BufferedWriter out = new BufferedWriter(file);
+
+        for(String[] problem : problemInfo){
+            out.write(problem[1] + "," + problem[2] + "," + problem[3] + "," + problem[4] + "\n");
+        }
+        out.close();
+    }
+    public List<String[]> getProblemInfo() throws IOException {
+        List<String[]> problems = new ArrayList<>();
+        getLinks(problems);
+        getAnswerTypes(problems);
+        getAnswerCount(problems);
+        return problems;
+    }
+    private void getLinks(List<String[]> problems) throws IOException{
+        Document doc = Jsoup.connect(
+                "https://practiceit.cs.washington.edu/problem/list").get();
+
+        Elements data = doc.getElementsByClass("problemLink");
+
+        int IDCounter = 2537;
+
+        for (Element problemInfo : data){
+            String[] linkInfo = new String[5];
+            String problemLink = problemInfo.attr("abs:href");
+            if(problemLink.contains("bjp4")){
+
+                linkInfo[0] = problemLink;
+                linkInfo[1] = String.valueOf(IDCounter);
+
+                if(problemLink.contains("/s")) linkInfo[2] = "sc";
+                else linkInfo[2] = "ex";
+
+                IDCounter++;
+                problems.add(linkInfo);
+            }
+        }
+    }
+    private void getAnswerTypes(List<String[]> problems) throws IOException {
+        for (String[] problem : problems) {
+            if(problem[2].equals("ex")) {
+                problem[3] = "codeSubmit";
+                continue;
+            }
+            Document doc = Jsoup.connect(problem[0]).get();
+            String problemType = answerType(doc);
+            problem[3] = problemType;
+        }
+    }
+    private String answerType(Document doc) {
+        if (doc.select("input.multiplechoiceanswer[name=mechanical1[]]").size() != 0) {
+            return "multipleSelect";
+        } else if (doc.select("td.multiplechoice").size() != 0) {
+            return "multipleChoice";
+        } else if (doc.select("input.expressionanswer[name=mechanical2]").size() != 0) {
+            return "multipleResponse";
+        }else if (doc.select("input.expressionanswer[name=mechanical1]").size() != 0){
+            return "freeResponse";
+        }else{
+            return "codeSubmit";
+        }
+    }
+    private void getAnswerCount(List<String[]> problems) throws IOException {
+        for (String[] problem : problems) {
+            if(Objects.equals(problem[3], "freeResponse") || Objects.equals(problem[3], "codeSubmit")){
+                problem[4] = "1";
+                continue;
+            }
+            Document doc = Jsoup.connect(problem[0]).get();
+            if(Objects.equals(problem[3], "multipleResponse")){
+                problem[4] = responseCount(doc);
+            }else{
+                problem[4] = multipleChoiceCount(doc);
+            }
+        }
+    }
+    private String responseCount(Document doc){
+        int checkCounter = 1;
+        while(true){
+            if(doc.select("input.expressionanswer[name=mechanical" + checkCounter + "]").size() != 0){
+                checkCounter++;
+            }else{
+                checkCounter--;
+                break;
+            }
+        }
+        return Integer.toString(checkCounter);
+    }
+    private String multipleChoiceCount(Document doc){
+        int checkCounter = 1;
+        while(true){
+            if(doc.select("input.multiplechoiceanswer[value=answer" + checkCounter + "]").size() != 0){
+                checkCounter++;
+            }else break;
+        }
+        return Integer.toString(checkCounter);
+    }
+}
---
+++ src/main/java/Submit.java
@@ -0,0 +1,123 @@
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Submit {
+    // self check - https://www.buildingjavaprograms.com/self-check-solutions-4ed.html
+    // exercies - https://github.com/ramakastriot/practiceit/tree/master/chapter2
+
+    public static void main(String[] args) throws IOException {
+
+        Submit submit = new Submit();
+
+        String url = "src/main/problemInfo";
+
+        Queue<String[]> problemInfo = submit.readProblemInfo(url);
+
+
+
+        //testing with multipleselect on 2.1 self-check
+        String[] test = {"2585","sc","multipleSelect","8"};
+
+        try(WebClient webClient = submit.login()){
+            submit.multipleSelectBruteForce(test, webClient);
+        }
+        /*
+        String q = "random word £500 bank $";
+        String url = "https://example.com?q=" + URLEncoder.encode(q, StandardCharsets.UTF_8);
+         */
+
+    }
+    public WebClient login() throws IOException{
+        try (final WebClient webClient = new WebClient()) {
+
+            webClient.getOptions().setCssEnabled(true);
+            webClient.getOptions().setJavaScriptEnabled(true);
+
+            final String email = "imaspacecatperson@gmail.com";
+            final String password = "thisisthepasswordbecareful";
+
+            final HtmlPage loginPage = webClient.getPage(
+                    "https://practiceit.cs.washington.edu/login");
+
+            final HtmlForm loginForm = loginPage.getHtmlElementById("loginform");
+
+            HtmlTextInput userField = loginForm.getInputByName("usernameoremail");
+            HtmlPasswordInput passField = loginForm.getInputByName("password");
+
+            userField.type(email);
+            passField.type(password);
+
+            HtmlSubmitInput submitInput = loginForm.getInputByName("login");
+            submitInput.click();
+            return webClient;
+        }
+    }
+    //problemInfo is problemId, sc/ex, problem type, number of answers
+    public Queue<String[]> readProblemInfo(String fileName) throws IOException {
+        FileReader file = new FileReader(fileName);
+        BufferedReader out = new BufferedReader(file);
+        Queue<String[]> problemInfo = new LinkedList<>();
+        String inputLine;
+        while((inputLine = out.readLine()) != null){
+            String[] inputValues = inputLine.split(",");
+            problemInfo.add(inputValues);
+        }
+        return problemInfo;
+    }
+    public void submitAnswers(Queue<String[]> problems, Queue<Queue<String>> scAnswers, Queue<String> exAnswers) throws IOException {
+
+        for(String[] problemInfo : problems){
+            if(problemInfo[3].equals("multipleChoice")){
+                //multipleChoiceBruteForce();
+            }else if(problemInfo[3].equals("multipleSelect")){
+                return;
+            }else if(problemInfo[3].equals("multipleResponse")){
+                continue;
+            }else{
+                continue;
+            }
+        }
+    }
+    public void multipleChoiceBruteForce(String[] problemInfo, WebClient webClient) throws IOException {
+        String link = "https://practiceit.cs.washington.edu/test/enqueue-job?problemid=";
+        link += problemInfo[0] + "&cheated=0&mechanical1=answer";
+
+        int numberOfIterations = Integer.parseInt(problemInfo[3]);
+        String submitURL;
+        for(int i = 0; i < numberOfIterations; i++){
+            submitURL = link+i;
+            webClient.getPage(submitURL);
+        }
+    }
+    public void multipleSelectBruteForce(String[] problemInfo, WebClient webClient) throws IOException{
+        String url = "https://practiceit.cs.washington.edu/test/enqueue-job?problemid=";
+        url += problemInfo[0] + "&cheated=0";
+
+        multipleSelectRecursive(webClient, 0, Integer.parseInt(problemInfo[3]), url);
+    }
+    private void multipleSelectRecursive(WebClient webClient, int min, int max, String url) throws IOException{
+        String newURL;
+        while(min<max){
+            newURL = url + "&mechanical1=answer" + min;
+            min++;
+            //System.out.println(newURL);
+            webClient.getPage(newURL);
+            multipleSelectRecursive(webClient, min, max, newURL);
+        }
+    }
+
+
+
+}
+
+
